ef07c7d7fe7eb061b9a9b836215d916d8e93093a
Kevin Gao <kevinzhugao@gmail.com>
2021-10-02 13:35:24 -0700

    Main endpoint responds with the correct response object, set up endpoints for furture functionality

--- database/index.js
+++ database/index.js
@@ -8,8 +8,29 @@ const pool = new Pool({
   port,
 });
 
-const retrieveProductQA = () => pool.query('SELECT question_id, question_body, question_date, asker_name, helpfulness, reported FROM questions WHERE product_id = 1');
+const productQAQuery = {
+  name: 'product-qa-query',
+  text: 'SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported FROM questions WHERE product_id = $1',
+}
+
+const aQuery = {
+  name: 'a-query',
+  text: 'SELECT answer_id, question_id, body, date, answerer_name, helpfulness FROM answers WHERE question_id = $1',
+}
+
+const pQuery = {
+  name: 'p-query',
+  text: 'SELECT id, url FROM answers_photos WHERE answer_id = $1',
+}
+
+const fetchProductQ = (productId) => pool.query(productQAQuery, [productId]);
+
+const fetchA = (questionId) => pool.query(aQuery, [questionId]);
+
+const fetchP = (answerId) => pool.query(pQuery, [answerId]);
 
 module.exports = {
-  retrieveProductQA,
+  fetchProductQ,
+  fetchA,
+  fetchP,
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,65 @@
 const app = require('express')();
-const { retrieveProductQA } = require('../database/index.js');
+const { fetchProductQ, fetchA, fetchP } = require('../database/index.js');
 
-app.get('/hello', (req, res) => {
-  retrieveProductQA()
-    .then(({ rows }) => console.log(rows))
-  res.end();
-})
+app.get('/qa/questions/:product_id', (req, res) => {
+  fetchProductQ(req.params.product_id)
+    .then(({ rows }) => (
+      rows.forEach((question, i) => (
+       fetchA(question.question_id)
+          .then((answer) => {
+            question.answers = {};
+            answer.rows.forEach((answer) => {
+              question.answers[answer.answer_id] = answer;
+              return fetchP(answer.answer_id)
+                .then((photo) => {
+                  answer.photos = photo.rows;
+                  if (i === rows.length - 1) res.send({product_id: req.params.product_id, results: rows})
+                })
+                .catch((err) => console.log(err))
+            })
+          })
+          .catch((err) => console.log(err))
+      ))
+    ))
+    .catch((err) => console.log(err))
+});
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  fetchA(req.params.question_id)
+    .then(({ rows }) => {
+      rows.forEach((answer, i) => {
+        fetchP(answer.answer_id)
+          .then((photos) => answer.photos = photos.rows)
+          .catch((err) => console.log(err))
+        if (i === rows.length - 1) res.send({question_id: req.params.question_id, results: rows})
+      })
+    })
+    .catch((err) => console.log(err))
+});
+
+app.post('/qa/questions', (req, res) => {
+
+});
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+
+});
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+
+});
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+
+});
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+
+});
+
+app.put('qa/asnwers/:answer_id/report', (req, res) => {
+
+});
 
 const PORT = 3000;
 
